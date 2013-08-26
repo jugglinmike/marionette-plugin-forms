72527eedd473821f8093f9d7cc4000aab2191786
Mike Pennisi <mike@mikepennisi.com>
2013-08-26 13:53:36 -0400

    Implement first signature of Form#fill

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ function Forms(client) {
   this.client = client;
 }
 
+function padZeros(val) {
+  val = String(val);
+  while (val.length < 2) {
+    val = '0' + val;
+  }
+  return val;
+}
+
+var dateFormatters = {
+  time: function (dateObj) {
+    return [dateObj.getHours(), dateObj.getMinutes(), dateObj.getSeconds()]
+      .map(padZeros)
+      .join(':');
+  },
+  date: function(dateObj) {
+    return [dateObj.getFullYear(), dateObj.getMonth() + 1, dateObj.getDate()]
+      .map(padZeros)
+      .join('-');
+  }
+};
+
 /**
  * Fill out one or more input elements (regardless of visibility)
  * @param {Element} elem The input element to change (when setting a single
@@ -11,13 +32,19 @@ function Forms(client) {
  *                       hash of form element names to corresponding values
  *                       (when setting multiple values)
  */
-Forms.prototype.fill = function(elem, values) {
-  setValue.call(this, elem, value);
+Forms.prototype.fill = function(elem, value, done) {
+  setValue.call(this, elem, value, done);
 };
 
-function setValue(elem, value) {
-  elem.scriptWith(function(elem) {
-    elem.value = value;
+function setValue(elem, value, done) {
+  elem.getAttribute('type', function(err, type) {
+    type = type.trim().toLowerCase();
+    if (value instanceof Date && dateFormatters.hasOwnProperty(type)) {
+      value = dateFormatters[type](value);
+    }
+    elem.client.executeScript(function(elem, value) {
+      elem.value = value;
+    }, [elem, value], done);
   });
 }
 
--- test/tests/index.js
+++ test/tests/index.js
@@ -1,8 +1,93 @@
 suite('Marionette Forms plugin', function() {
+  var fork = require('child_process').fork;
+  var emptyPort = require('empty-port');
+  var async = require('async');
+
   var client = createClient();
+  var serverAddr, serverProcess;
+
   marionette.plugin('forms', require('../..'));
 
-  test('#fill', function() {
-    assert.ok(typeof client.forms.fill === 'function');
+  suiteSetup(function(done) {
+    emptyPort({ startPort: 3000 }, function(err, port) {
+      serverProcess = fork(__dirname + '/../server.js', [], {
+        env: { PORT: port }
+      });
+      serverAddr = 'http://localhost:' + port;
+      done(err);
+    });
+  });
+
+  suiteTeardown(function() {
+    serverProcess.kill();
+  });
+
+  setup(function(done) {
+    client.goUrl(serverAddr, function(err) {
+      async.parallel([
+        client.findElement.bind(client, '#form-a'),
+        client.findElement.bind(client, '[name="my-text-1"]'),
+        client.findElement.bind(client, '[name="my-text-2"]'),
+        client.findElement.bind(client, '[name="my-time"]'),
+        client.findElement.bind(client, '[name="my-date"]')
+      ], function(err, results) {
+        this.form = results[0];
+        this.text1 = results[1];
+        this.text2 = results[2];
+        this.time = results[3];
+        this.date = results[4];
+        done();
+      }.bind(this));
+    }.bind(this));
+  });
+
+  suite('#fill', function() {
+
+    suite('single element', function() {
+      test('text', function(done) {
+        client.forms.fill(this.text1, 'Some text', function() {
+          this.text1.getAttribute('value', function(err, val) {
+            assert.equal(val, 'Some text');
+            done();
+          });
+        }.bind(this));
+      });
+      test('time', function(done) {
+        var now = new Date();
+        now.setHours(1);
+        now.setMinutes(2);
+        now.setSeconds(3);
+        client.forms.fill(this.time, now, function() {
+          this.time.getAttribute('value', function(err, val) {
+            assert.equal(val, '01:02:03');
+            done();
+          });
+        }.bind(this));
+      });
+      test('date', function(done) {
+        var now = new Date();
+        now.setYear(1997);
+        now.setMonth(0);
+        now.setDate(2);
+        client.forms.fill(this.date, now, function() {
+          this.date.getAttribute('value', function(err, val) {
+            assert.equal(val, '1997-01-02');
+            done();
+          });
+        }.bind(this));
+      });
+    });
+
+    test.skip('multiple elements', function() {
+      //assert.ok(typeof client.forms.fill === 'function');
+      client.forms.fill(this.form, {
+        'my-text-1': 'Some text',
+        'my-text-2': 'Some more text'
+      });
+      assert.equal(
+        client.findElement('#form-a [name="my-text-1"]').getAttribute('value'),
+        'Some text'
+      );
+    });
   });
 });
