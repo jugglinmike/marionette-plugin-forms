00a8e76b33c1a209b3565c9b94f1cb969ce55da9
Mike Pennisi <mike@mikepennisi.com>
2013-08-27 19:06:24 -0400

    Document date and time formats

--- src/date-formatters.js
+++ src/date-formatters.js
@@ -7,11 +7,19 @@ function padZeros(val) {
 }
 
 module.exports = {
+  // As per [the "time" input element's
+  // specification](http://dev.w3.org/html5/markup/input.time.html), this value
+  // is formatted according to [RFC
+  // 3339](http://tools.ietf.org/html/rfc3339#section-5.6).
   time: function (dateObj) {
     return [dateObj.getHours(), dateObj.getMinutes(), dateObj.getSeconds()]
       .map(padZeros)
       .join(':');
   },
+  // As per [the "date" input element's
+  // specification](http://dev.w3.org/html5/markup/input.date.html), this value
+  // is formatted according to [RFC
+  // 3339](http://tools.ietf.org/html/rfc3339#section-5.6).
   date: function(dateObj) {
     return [dateObj.getFullYear(), dateObj.getMonth() + 1, dateObj.getDate()]
       .map(padZeros)
